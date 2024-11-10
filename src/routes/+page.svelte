32fc64026852c5a227e79235b60a75d42b587936
Jonnybvb76 <jonnybvb76@gmail.com>
2024-11-10 15:25:50 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    let length = $state(0);
+    let weight = $state(0);
+    let result = $state(0);
+
+    function calculate() {
+        result = weight / ((length) ** 2)
+    }
+</script>
+
+<input bind:value={length} type="text" placeholder="Körpergröße"><br>
+<input bind:value={weight} type="text" placeholder="Gewicht"><br>
+
+<button onclick={calculate}>Berechnen</button>
+
+<h1>{result}</h1>
